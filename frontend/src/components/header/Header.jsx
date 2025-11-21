7c40b381563fd7f6bea7da5fa7125fc2cbba843a
mnawroc1 <01187259@pw.edu.pl>
2025-11-21 18:51:34 +0100

    Refactor navigation in header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -6,21 +6,29 @@ import './Header.css'
 
 
 export default function Header() {
-    const { isLoggedIn, login, logout } = useAuth();
 
     return (
         <div className="header-container">
             <div className='header-logo-div'>Logo placeholder</div>
-            <div className='header-navigation-div'>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/profile">Profile</NavLink>
-                {isLoggedIn ? (
-                    <button onClick={logout}>Logout</button>
-                ) : (
-                    <button onClick={login}>Login</button>
-                )}
-            </div>
+            <Navigation />
+        </div>
+    )
+}
+
+
+function Navigation() {
+    const { isLoggedIn, login, logout } = useAuth();
+
+    return (
+        <div className='header-navigation-div'>
+            <NavLink to="/" className={"header-navigation-button"}>Home</NavLink>
+            <NavLink to="/about" className={"header-navigation-button"}>About</NavLink>
+            <NavLink to="/profile" className={"header-navigation-button"}>Profile</NavLink>
+            {isLoggedIn ? ( 
+                <button onClick={logout} className={"header-navigation-button"}>Logout</button> //TODO: These buttons arent styled properly
+            ) : (
+                <button onClick={login} className={"header-navigation-login-button"}>Login</button>
+            )}
         </div>
     )
 }
