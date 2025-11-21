d1585bd0066a27f9752fbe5d7c8389c6ba5f6ddd
mnawroc1 <01187259@pw.edu.pl>
2025-11-21 18:19:09 +0100

    Add AppContent with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+
 import Header from './components/header/Header'
+import AppContent from './components/appContent/AppContent.jsx'
+
+import './App.css'
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <Header />
-            <p>Hello Vite + React!</p>
-        </>
+            <AppContent />
+        </BrowserRouter>
     )
 }
 
---
+++ frontend/src/components/appContent/AppContent.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+
+import './AppContent.css'
+
+
+export default function AppContent() {
+    return (
+        <div className="app-content-container">
+            <Routes>
+                { // TODO: Add routes
+}
+                <Route path="/" element={<div>Home page</div>} />
+                <Route path="/about" element={<div>About page</div>} />
+                <Route path="/profile" element={<div>Profile page</div>} />
+                <Route path="*" element={<div>404 — Not Found</div>} />
+            </Routes>
+        </div>
+    )
+}
