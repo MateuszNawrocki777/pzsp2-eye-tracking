7311fd70b04695b14e9f981bf47c31c99fb5c478
mnawroc1 <01187259@pw.edu.pl>
2025-11-21 18:36:26 +0100

    Add Navigation buttons

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom'
+
 import './Header.css'
 
 
@@ -5,7 +7,11 @@ export default function Header() {
     return (
         <div className="header-container">
             <div className='header-logo-div'>Logo placeholder</div>
-            <div className='header-navigation-div'>Navigation buttons?</div>
+            <div className='header-navigation-div'>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/profile">Profile</NavLink>
+            </div>
         </div>
     )
 }
