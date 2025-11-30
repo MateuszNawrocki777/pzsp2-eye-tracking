2c15959ed3d281e783ad5794346f5825a8ccc630
mnawroc1 <01187259@pw.edu.pl>
2025-12-01 00:45:15 +0100

    Change username to email in login page

--- frontend/src/components/appContent/loginPage/LoginPage.jsx
+++ frontend/src/components/appContent/loginPage/LoginPage.jsx
@@ -12,18 +12,18 @@ export default function LoginPage() {
     const navigate = useNavigate();
     const { login } = useAuth();
 
-    const usernameRef = useRef();
+    const emailRef = useRef();
     const passwordRef = useRef();
 
     const [error, setError] = useState("");
 
     const handleLogin = async () => {
-        const username = usernameRef.current.value;
+        const email = emailRef.current.value;
         const password = passwordRef.current.value;
         try {
-            await login(username, password);
+            await login(email, password);
         } catch (error) {
-            setError("Login failed.");
+            setError("Incorrect email or password");
             return;
         }
         navigate("/");
@@ -33,7 +33,7 @@ export default function LoginPage() {
         <div className="login-page-container">
             <h1>Login</h1>
             <div className='login-page-content'>
-                <input type="text" placeholder="Username" ref={usernameRef} />
+                <input type="text" placeholder="Email" ref={emailRef} />
                 <input type="password" placeholder="Password" ref={passwordRef} />
                 <LoadingButton className='login-page-login-button' onClick={handleLogin}>Login</LoadingButton>
                 <button onClick={() => {navigate("/register")}}>Register</button>
